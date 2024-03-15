04a931cb4ae55747b2bc42c3de3af5d8565b0dbb
Fabio Varela <fabio-varela@protonmail.com>
2024-03-15 00:29:09 -0300

    sync and fix #2

--- users/models.py
+++ users/models.py
@@ -98,23 +98,3 @@ class CustomUser(AbstractBaseUser, PermissionsMixin):
 
 
 
-<<<<<<< HEAD
-class Address(db.Model):
-    name = db.CharField(max_length=75, blank=False, null=False)
-    street = db.CharField(max_length=75, blank=False, null=False)
-    neighborhood = db.CharField(max_length=75, blank=False, null=False)
-    number = db.CharField(max_length=75, blank=False, null=False)
-    complement = db.CharField(max_length=75, blank=True, null=True)
-    city = db.CharField(max_length=75, blank=False, null=False)
-    state = db.CharField(max_length=75, blank=False, null=False)
-    zip_code = db.CharField(max_length=8, blank=False, null=False)
-
-    user = db.ForeignKey(CustomUser, on_delete=db.SET_NULL, null=True, blank=False)
-
-    is_active = db.BooleanField(default=True)
-
-
-    def __str__(self):
-        return self.name
-=======
->>>>>>> fc2d1810266d1d99bf81c5d323c9430780531230
